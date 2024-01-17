a71107a62b54075109585c7dee09f3b07fb5a1bd
shadmehr <y.shadmehr@gmail.com>
2024-01-17 20:06:48 +0000

    docker_TR

---
+++ docker-challenge/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+        source  = "kreuzwerker/docker"
+        version = "~> 2.22.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "simplegoservice" {
+    name = "registry.gitlab.com/alta3/simplegoservice"
+}
+
+resource "docker_container" "simplegoservice" {
+
+    image = docker_image.simplegoservice.image_id
+    name = var.container_name
+ 
+    ports {
+        internal = var.internal_port
+        external = var.external_port
+    }
+}
+
