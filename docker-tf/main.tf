9d9adc4388a4f4f97ce126489d4e9447b97ad0e2
shadmehr <y.shadmehr@gmail.com>
2024-01-17 16:46:10 +0000

    terraform and docker

---
+++ docker-tf/main.tf
@@ -0,0 +1,32 @@
+# Terraform Block
+terraform {
+ required_providers {
+    // this is docker priovder
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+
+  /* this is my
+                Comment
+  */
+
+  name         = "nginx:1.19.6"
+  keep_locally = true    // keep image after "destroy"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 2224
+  }
+}
+
